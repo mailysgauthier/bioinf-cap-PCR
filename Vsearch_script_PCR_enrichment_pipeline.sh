180d235c6c3b612bbffebefafbddb3ff43be6c71
mailysgauthier <48444616+mailysgauthier@users.noreply.github.com>
2021-10-06 09:18:38 +0200

    Update Vsearch_script_PCR_enrichment_pipeline.sh

--- Vsearch_script_PCR_enrichment_pipeline.sh
+++ Vsearch_script_PCR_enrichment_pipeline.sh
@@ -122,14 +122,14 @@ for f in *Fwh*_R1_001.fastq; do
 	echo
     echo Calculate quality statistics
     $VSEARCH --threads $THREADS \
-             --fastq_eestats $merge/$s.merged.fastq \
-             --output $merge/$s.stats
+             --fastq_eestats $trim/$s.trimmed.fastq \
+             --output $trim/$s.stats
 
     echo
     echo Quality filtering
 
     $VSEARCH --threads $THREADS \
-             --fastq_filter $merge/$s.merged.fastq \
+             --fastq_filter $trim/$s.trimmed.fastq  \
              --fastq_maxee 1 \
              --fastq_minlen 150 \
              --fastq_maxlen 250 \
